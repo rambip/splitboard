bc77799b57bf49e5f902e27d0c26397cef07f658
Antonin Peronnet <apero1808@gmail.com>
2021-08-01 19:46:25 +0200

    personal config

--- code/keyboard/config.h
+++ code/keyboard/config.h
@@ -24,30 +24,31 @@ look inside OS_layouts.h
 const char _base_layer[36] PROGMEM = 
 {
     // TODO: debug "`" and "^" key 
-'\t','b','é','p','o','w',      0x80,'v','d','l','j',C_BS,
+'\t','b','é','p','o',0x80,      'w','v','d','l','j',C_BS,
 C_ES,'a','u','i','e',',',      'c','t','s','r','n','m',
-C_DEL ,'z','y','x','.','k',     '\'','q','g','h','f','\n',
+C_DEL ,'z','y','x','.','\'',     'k','q','g','h','f','\n',
 };
 
 const char _shift_layer[36] PROGMEM = 
 {
-'\t','B','É','P','O','W',      0x81,'V','D','L','J',C_BS,
+    // é maj FIXME
+'\t','B','É','P','O',0x81,      'W','V','D','L','J',C_BS,
 C_ES,'A','U','I','E',';',      'C','T','S','R','N','M',
-C_DEL ,'Z','Y','X',':','K',     '\"','Q','G','H','F','\n',
+C_DEL ,'Z','Y','X',':','\"',     'K','Q','G','H','F','\n',
 };
 
 const char _symb_layer[36] PROGMEM = 
 {
-    '\t', 0,'@','#','$','%',   '^','&','*','(',')',C_BS,
-    C_ES, 0, 0,0,C_LEFT, C_RIGHT,      0  ,'[',']','{','}','\n',
-    C_DEL,  0, 0, 0, 0, 0,     0, 0, '<', '>', 0, 0, 
+    '\t', '!' ,'@','#','$','%',        '^','&','*','(',')',C_BS,
+C_ES,C_LEFT,C_UP,C_DOWN,C_RIGHT,',',   'ç','[',']','{','}','|',
+    C_DEL, '?', C_HOME,C_END,'.', '`',         0, '<', '>', '~', '/', 0, 
 };
 
 const char _num_layer[36] PROGMEM = 
 {
     '\t','1','2','3','4','5',   '6','7','8','9','0',C_BS,
-    C_BS, 0, 0, 0, 0, 0,        C_HOME,'4','5','6',C_END, '\n',
-    C_DEL, 0, 0, 0, 0,           '0',   '1','2','3',0, 0, 
+    C_BS,'=',0,'+','-',',',        0,'4','5','6','0', '\n',
+    C_DEL, 0, 0, 0, '.', 0,       '0',   '1','2','3','/',0,  
 };
 
 
@@ -64,17 +65,17 @@ const char _num_layer[36] PROGMEM =
 
 // layering keys (to access the corresponding layers)
 #define SYMB_LAYER 40
-#define SHIFT_LAYER 41
+#define SHIFT_LAYER 42
 #define NUM_LAYER 43
 #define MM_LAYER 37
 #define MACRO_LAYER 46
 
 // modifiers keys (are treated after, only for the OS)
 #define MODIFIER_ALT 39
-#define MODIFIER_SUPER 40
+#define MODIFIER_SUPER 38
 #define MODIFIER_CTRL 44
 
 // space key
-#define SPACE 42
+#define SPACE 41
 
 
